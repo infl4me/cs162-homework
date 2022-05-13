0cb13c98a7709b97dd319089d95b600d7183e0d7
infl4me <hellpl4y@gmail.com>
2022-05-13 13:05:46 +0000

    [hw0] add limits

--- hw-intro/limits.c
+++ hw-intro/limits.c
@@ -2,9 +2,12 @@
 #include <sys/resource.h>
 
 int main() {
-    struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
-    return 0;
+  struct rlimit lim;
+  getrlimit(RLIMIT_STACK, &lim);
+  printf("stack size. cur: %llu; max: %llu\n", lim.rlim_cur, lim.rlim_max);
+  getrlimit(RLIMIT_NPROC, &lim);
+  printf("process limit: %llu\n", lim.rlim_max);
+  getrlimit(RLIMIT_NOFILE, &lim);
+  printf("max file descriptors: %llu\n", lim.rlim_max);
+  return 0;
 }
